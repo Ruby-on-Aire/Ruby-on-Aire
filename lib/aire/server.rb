92fbed1e0ee9fc992e28bfe605263997964a0c48
Jens Dill <76878132+JensMDill@users.noreply.github.com>
2021-01-17 11:02:02 -0500

    Create server.rb
    Initial server just displays the About page.

---
+++ lib/aire/server.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+#  Copyright (C) 2021 Jens M. Dill
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+module Aire
+
+  # The Aire::Server is the main entry point for the Aire Web Server. 
+  # It conforms to the interface described in the <a href="https://github.com/rack/rack/blob/master/SPEC.rdoc">Rack specification</a>.
+  #
+  class Server
+  
+    def call (env)
+      # For now, just return the About page.
+      [ 200, {"Content-Type" => "text/plain"}, [Aire::About.value] ]
+    end
+  
+  end # Server
+  
+end # Aire
